98d87cf529bbaef6a81929b2d43a3018303a3b2c
Nicole Leon <nicoleleonux@gmail.com>
2023-11-07 15:38:56 -0500

    category

---
+++ FinalProject.DAL/Models/AdventureWorksSettings.cs
@@ -0,0 +1,9 @@
+namespace FinalProject.DAL.Models
+{
+    public class AdventureWorksSettings
+    {
+        public string categoryUrl { get; set; } = string.Empty;
+        public string productsUrl { get; set; } = string.Empty;
+        public string suppliersUrl { get; set; } = string.Empty;
+    }
+}
---
+++ FinalProject.DAL/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace FinalProject.DAL.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
