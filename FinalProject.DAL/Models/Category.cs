b0d1cd35ac8ee26ed3b428eeef64bbca43134941
Nicole Leon <nicoleleonux@gmail.com>
2023-11-09 12:04:21 -0500

    ready for thursday class

--- FinalProject.DAL/Models/Category.cs
+++ FinalProject.DAL/Models/Category.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace FinalProject.DAL.Models
 {
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string Description { get; set; }
-    }
+	public class Category
+	{
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public string Description { get; set; }
+	}
 }
