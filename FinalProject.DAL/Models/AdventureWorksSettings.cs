40ae3f5e8ffd5403ff9d95134f3123618fcf81bb
nfv5075 <nfv5075@psu.edu>
2023-11-28 15:26:52 -0500

    fix my side

--- FinalProject.DAL/Models/AdventureWorksSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalProject.DAL.Models
-{
-    public class AdventureWorksSettings
-    {
-        public string categoryUrl { get; set; } = string.Empty;
-        public string productsUrl { get; set; } = string.Empty;
-        public string suppliersUrl { get; set; } = string.Empty;
-    }
-}
