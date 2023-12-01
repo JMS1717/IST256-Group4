0875fc9dfc412b7f0362a5f243e7118add143bfe
Nicole Leon <nvl5303@psu.edu>
2023-12-01 15:21:13 -0500

    quick fixes/cosmetic changes

--- FinalProject.DAL/Models/AdventureWorksSettings.cs
+++ FinalProject.DAL/Models/AdventureWorksSettings.cs
@@ -3,7 +3,5 @@ namespace FinalProject.DAL.Models
     public class AdventureWorksSettings
     {
         public string categoryUrl { get; set; } = string.Empty;
-        public string productsUrl { get; set; } = string.Empty;
-        public string suppliersUrl { get; set; } = string.Empty;
     }
 }
